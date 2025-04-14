50ad7e955cc97b2f6ccc60bbce58104bf0c6dc19
PatrioticCobra <140697623+PatrioticCobra@users.noreply.github.com>
2025-04-14 18:15:03 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,1 +1,18 @@
+from socket import *
 
+HOST = "172.21.12.32" # Host IP Address
+PORT = 12345 # Host port number
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)  # Create UDP socket
+clientSocket.sendto("Wagwan".encode(), (HOST, PORT))  # Message to register with the server
+print("Client is online and waiting for alerts...")
+
+try:
+    while True:  
+        Alert, serverAddress = clientSocket.recvfrom(2048) # Server message 
+        print(Alert.decode()) # Printing server message
+
+except KeyboardInterrupt:
+    print("\nClient shutting down...")
+finally:
+    clientSocket.close()
